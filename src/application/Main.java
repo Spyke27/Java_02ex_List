5feb3851e1409c7bb6e2aa1952b6c7cdfc9e06e1
Spyke27 <rodrigoeufrasio2@gmail.com>
2023-08-02 22:48:57 -0300

    ArrayList add and remove elements

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,10 +2,12 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 		
 		//Dont acept primitives types
 		List<String> list = new ArrayList<>();
@@ -14,6 +16,16 @@ public class Main {
 		list.add("Joao");
 		list.add("Ronaldo");
 		list.add("Neymar");
+		
+		for(String nome: list) {
+			System.out.println(nome);
+		}
+		System.out.println();
+		
+		System.out.print("Digite o nome que deseja deletar: ");
+		String nomeToRemove = scan.next();
+		list.remove(nomeToRemove);
+		System.out.println();
 		
 		for(String nome: list) {
 			System.out.println(nome);
